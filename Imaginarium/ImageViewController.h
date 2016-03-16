b194833264f8fc633caec59b39e89cac0e44390e
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-03-16 10:30:23 -0700

    added spinner, loading image oof main queue

---
+++ Imaginarium/ImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by David Anglin on 3/16/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (strong, nonatomic) NSURL *imageURL;
+
+
+@end
